85cd864753f93f442f223be9447d3ef5aae5728d
Patrick Simpson <izerop@gmail.com>
2024-05-24 16:46:50 -0400

    fix: updates

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.g.mapleader = ","
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 require("config.lazy")
 
 local neogit = require("neogit")
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { "RRethy/nvim-treesitter-endwise" },
     opts = function(_, opts)
       opts.endwise = { enable = true }
-      opts.indent = { enable = true, disable = { "yaml", "ruby" } }
+      opts.indent = { enable = true, disable = { "yaml" } }
       opts.ensure_installed = {
         "bash",
         "embedded_template",
